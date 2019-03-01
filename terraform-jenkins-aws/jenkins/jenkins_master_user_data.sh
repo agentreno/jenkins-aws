e914d60b3e03fcd65c137ce9c62a1da5c4f041db
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-03-01 10:39:47 +0000

    Install plugins automatically

--- terraform-jenkins-aws/jenkins/jenkins_master_user_data.sh
+++ terraform-jenkins-aws/jenkins/jenkins_master_user_data.sh
@@ -7,3 +7,24 @@ add-apt-repository "deb https://pkg.jenkins.io/debian-stable binary/"
 apt-get update
 apt-get -y install default-jre
 apt-get -y install jenkins
+
+# Install plugins
+until test -f /var/lib/jenkins/secrets/initialAdminPassword; do sleep 1; done
+JENKINS_ADMIN_PASSWORD=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
+checkJenkinsReady() {
+    java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar \
+        -s http://localhost:8080 \
+        -auth admin:$JENKINS_ADMIN_PASSWORD \
+        version
+}
+until checkJenkinsReady; do sleep 1; done
+
+PLUGINS=("ssh-slaves" "blueocean")
+for PLUGIN in "${PLUGINS[@]}"
+do
+	java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar \
+		-s http://localhost:8080 \
+		-auth admin:$JENKINS_ADMIN_PASSWORD \
+		install-plugin $PLUGIN \
+		-deploy
+done
